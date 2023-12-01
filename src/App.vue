9fe4e9ad8d3a1f80e7ca59bc1a53a822841d9de1
Yuna1002 <e2431666@gmail.com>
2023-12-01 15:00:52 +0800

    vite 環境建立

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <header></header>
+  <main>
+    <RouterView></RouterView>
+  </main>
+</template>
+
+<script>
+import { RouterView } from 'vue-router'
+export default {
+  components: {
+    RouterView
+  }
+}
+</script>
+
+<style scoped></style>
